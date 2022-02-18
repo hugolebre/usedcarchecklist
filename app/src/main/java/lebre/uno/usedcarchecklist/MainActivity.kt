809de27301fe57819d51ae6b608aba6868c3aaa3
hugolebre <hglebre@gmail.com>
2022-02-18 19:08:32 +0000

    Initial commit

---
+++ app/src/main/java/lebre/uno/usedcarchecklist/MainActivity.kt
@@ -0,0 +1,13 @@
+package lebre.uno.usedcarchecklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
